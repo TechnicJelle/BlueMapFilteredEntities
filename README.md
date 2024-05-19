5d5ab61b4ee8af83a9fcba195b1403d4bae5c750
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-05-19 18:00:04 +0200

    Initial commit, WIP

---
+++ README.md
@@ -0,0 +1,15 @@
+# BlueMap Filtered Entities
+
+[![GitHub Total Downloads](https://img.shields.io/github/downloads/TechnicJelle/BlueMapFilteredEntities/total?label=Downloads&color=success "Click here to download the plugin")](https://github.com/TechnicJelle/BlueMapFilteredEntities/releases/latest)
+[![Servers using this plugin](https://img.shields.io/bstats/servers/21976?label=Servers)](https://bstats.org/plugin/bukkit/BlueMap%20Filtered%20Entities/21976)
+
+Compatible with Paper 1.13+, just like BlueMap itself.
+
+To reload this plugin, just reload BlueMap itself with `/bluemap reload`.
+
+## [Click here to download!](../../releases/latest)
+
+## Support
+To get support with this plugin, join the [BlueMap Discord server](https://bluecolo.red/map-discord)
+and ask your questions in [#3rd-party-support](https://discord.com/channels/665868367416131594/863844716047106068).
+You're welcome to ping me, @TechnicJelle.
---
+++ src/main/java/com/technicjelle/bluemapfilteredentities/BlueMapFilteredEntities.java
@@ -0,0 +1,111 @@
+package com.technicjelle.bluemapfilteredentities;
+
+import com.flowpowered.math.vector.Vector3d;
+import com.technicjelle.UpdateChecker;
+import de.bluecolored.bluemap.api.BlueMapAPI;
+import de.bluecolored.bluemap.api.BlueMapMap;
+import de.bluecolored.bluemap.api.BlueMapWorld;
+import de.bluecolored.bluemap.api.markers.MarkerSet;
+import de.bluecolored.bluemap.api.markers.POIMarker;
+import org.bstats.bukkit.Metrics;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.function.Consumer;
+
+public final class BlueMapFilteredEntities extends JavaPlugin {
+	private UpdateChecker updateChecker;
+
+	@Override
+	public void onEnable() {
+		new Metrics(this, 21976);
+
+		updateChecker = new UpdateChecker("TechnicJelle", "BlueMapFilteredEntities", getDescription().getVersion());
+		updateChecker.checkAsync();
+
+		BlueMapAPI.onEnable(onEnableListener);
+		BlueMapAPI.onDisable(onDisableListener);
+	}
+
+	private final Consumer<BlueMapAPI> onEnableListener = api -> {
+		updateChecker.logUpdateMessage(getLogger());
+		getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+			long millisAtStart = System.currentTimeMillis();
+
+			List<World> worlds = getServer().getWorlds();
+			for (World world : worlds) {
+				BlueMapWorld blueMapWorld = api.getWorld(world).orElse(null);
+				if (blueMapWorld == null) {
+					getLogger().warning("Failed to get BlueMapWorld for world: " + world.getName());
+					continue;
+				}
+
+				if (blueMapWorld.getMaps().isEmpty()) continue;
+
+				getLogger().info("Adding markers for world: " + world.getName());
+				MarkerSet markerSet = MarkerSet.builder()
+						.label("Entities")
+						.toggleable(true)
+						.defaultHidden(true)
+						.build();
+
+				for (BlueMapMap map : blueMapWorld.getMaps()) {
+					map.getMarkerSets().put(map.getId() + "_entities", markerSet);
+				}
+
+				List<Entity> entities = world.getEntities();
+				markerSet.getMarkers().clear();
+				for (Entity entity : entities) {
+					if (entity instanceof Player) continue;
+
+					//TODO: Add special data for Item Frames
+					//TODO: Add special data for Armor Stands
+
+//					getLogger().info("Type: " + entity.getType());
+//					getLogger().info("Name: " + entity.getName());
+//					getLogger().info("UUID: " + entity.getUniqueId());
+//					getLogger().info("Spawn Reason: " + entity.getEntitySpawnReason());
+//					getLogger().info("Custom Name: " + getCustomName(entity));
+//					getLogger().info("Location: " + entity.getLocation());
+//					getLogger().info("Scoreboard Tags: " + String.join(", ", entity.getScoreboardTags()));
+//					getLogger().info("------------------------------");
+
+					Vector3d position = new Vector3d(entity.getLocation().getX(), entity.getLocation().getY(), entity.getLocation().getZ());
+					POIMarker marker = POIMarker.builder()
+							.label(entity.getName())
+							.position(position)
+							.build();
+					markerSet.put(entity.getUniqueId().toString(), marker);
+				}
+			}
+			getLogger().info("Took " + (System.currentTimeMillis() - millisAtStart) + "ms to add entity markers for all worlds.");
+		}, 0, 20 * 10);
+	};
+
+	@Nullable
+	String getCustomName(Entity entity) {
+		if (entity.getType() == EntityType.DROPPED_ITEM) {
+			Item item = (Item) entity;
+			ItemMeta itemMeta = item.getItemStack().getItemMeta();
+			if (itemMeta.hasDisplayName()) {
+				return itemMeta.getDisplayName();
+			}
+		}
+
+		return entity.getCustomName();
+	}
+
+	private final Consumer<BlueMapAPI> onDisableListener = api -> getServer().getScheduler().cancelTasks(this);
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
