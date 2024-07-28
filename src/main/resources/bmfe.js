dda26cf008af2d03f08f68d96287f7b0ecf34722
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-07-28 18:30:51 +0200

    Prevent js collisions

--- src/main/resources/bmfe.js
+++ src/main/resources/bmfe.js
@@ -1,30 +1,32 @@
-const { PoiMarker, animate, EasingFunctions } = window.BlueMap;
-const original = PoiMarker.prototype.updateFromData;
-PoiMarker.prototype.updateFromData = function (markerData) {
-    if (!(this.data?.id ?? "").startsWith("bmfe.") || (!this.position.x && !this.position.y && !this.position.z)) {
-        return original.call(this, markerData)
-    }
-    let startPos = {
-        x: this.position.x,
-        y: this.position.y,
-        z: this.position.z
-    }
-    original.call(this, {
-        ...markerData,
-        position: startPos
-    })
-    let pos = markerData.position || {}
-    let deltaPos = {
-        x: (pos.x || 0) - startPos.x,
-        y: (pos.y || 0) - startPos.y,
-        z: (pos.z || 0) - startPos.z,
-    }
-    if (deltaPos.x || deltaPos.y || deltaPos.z) {
-        animate(progress => {
-            let ease = EasingFunctions.easeInOutCubic(progress);
-            this.position.setX(startPos.x + deltaPos.x * ease || 0)
-            this.position.setY(startPos.y + deltaPos.y * ease || 0)
-            this.position.setZ(startPos.z + deltaPos.z * ease || 0)
-        }, 1000)
-    }
+{
+	const { PoiMarker, animate, EasingFunctions } = window.BlueMap;
+	const originalUpdateFromData = PoiMarker.prototype.updateFromData;
+	PoiMarker.prototype.updateFromData = function (markerData) {
+		if (!(this.data?.id ?? "").startsWith("bmfe.") || (!this.position.x && !this.position.y && !this.position.z)) {
+			return originalUpdateFromData.call(this, markerData)
+		}
+		let startPos = {
+			x: this.position.x,
+			y: this.position.y,
+			z: this.position.z
+		}
+		originalUpdateFromData.call(this, {
+			...markerData,
+			position: startPos
+		})
+		let pos = markerData.position || {}
+		let deltaPos = {
+			x: (pos.x || 0) - startPos.x,
+			y: (pos.y || 0) - startPos.y,
+			z: (pos.z || 0) - startPos.z,
+		}
+		if (deltaPos.x || deltaPos.y || deltaPos.z) {
+			animate(progress => {
+				let ease = EasingFunctions.easeInOutCubic(progress);
+				this.position.setX(startPos.x + deltaPos.x * ease || 0)
+				this.position.setY(startPos.y + deltaPos.y * ease || 0)
+				this.position.setZ(startPos.z + deltaPos.z * ease || 0)
+			}, 1000)
+		}
+	}
 }
